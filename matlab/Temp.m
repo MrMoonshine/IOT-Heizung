08b414c2b973c2dc3f46dded1fd8d79267e074d9
MrMoonshine <34010451+MrMoonshine@users.noreply.github.com>
2021-07-09 12:42:03 +0200

    Exponentielle NTC Formel
    Vergleich mit Exponentialfunktion zeigt dass die Polynomfunktion 5. Grades zu hohes Overfitting verursacht

--- matlab/Temp.m
+++ matlab/Temp.m
@@ -1,31 +1,80 @@
 %Author: David Monschein
-%License: GNU GPL 3
+%License: MIT
 %2020-11-15
 
 %Masuring results
-Rmat = [4760  4170  3150  2400  1700  1550  1340  1140  940 750];
-Tmat = [22    28    34    40    47    49    55    58    64  69];
-RTpolynom = polyfit(Rmat,Tmat,3);
+Rmat = [];
+Tmat = [];
+
+Rplotmax = 40000;
+
+csvResult = csvread("temp.csv");
+[csvHeight,csvWidth] = size(csvResult);
+%Load CSV
+for i = 1:(csvHeight/2)
+   Tmat = [Tmat csvResult(i,1)];
+   Rmat = [Rmat csvResult(i,2)];
+end
+%Create Regression
+RTpolynom = polyfit(Rmat,Tmat,5);
 PolyString = polyout(RTpolynom,'Rt')
+disp(PolyString)
+
+%Create Exponential
+Rtv = linspace(0,Rplotmax,100);
+Rtlog = logspace(0,5,100);
+Rtv
+R25 = 4530;
+B = 4048.76;
+tex = linspace(-20,100,100);
+k = 273.15;
+T25 = 25 + k;
+%Rntc = R25*(R25.^((1 ./ (tex+273.15))-(1 ./ (25+273.15))));
+
+Rntc = R25 .* exp(B .* ((1 ./ (tex .+ k))-(1/T25)));
+%This line Works somehow...
+Tntc = (1 ./ ((log(Rtlog ./ R25) ./ B) .+ 1/T25)) .- k;
+%Tntc = 25 .* exp((B .* ((1 ./ Rtv) .- (1 / R25)))) .- 16;
 
 figure(1)
-%Shuow Measuring Results
+%Show Measuring Results
 subplot(2,1,1);
-stem(Tmat,Rmat,'linewidth',2,'color','red','filled');
+stem(Rmat,Tmat,'linewidth',2,'color','red','filled');
 title('Messwerte der Temperaturfühler');
-ylabel('R [Ohm]');
-xlabel('T [°C]');
-grid on;
-
-Rtv = linspace(0,6000,100);
-subplot(2,1,2);
-plot(Rtv,polyval(RTpolynom,Rtv),'linewidth',2);
-title({"Polynomfunktion";PolyString;});
-
 ylabel('T [°C]');
 xlabel('R [Ohm]');
+grid on;
+
+subplot(2,1,2);
+plot(Rtv ,polyval(RTpolynom,Rtv),'linewidth',2);
 hold on
 scatter(Rmat,Tmat,'filled');
 hold off
 grid on
 legend('Polynomfunktion','Messwerte');
+ylabel('T [°C]');
+xlabel('R [Ohm]');
+title({"Polynomfunktion";PolyString;});
+%Exponential
+figure(2)
+subplot(2,1,1);
+plot(tex ,Rntc,'linewidth',2,'color','magenta');
+hold on
+scatter(Tmat,Rmat,'filled');
+hold off
+grid on
+legend('Exponentialfunktion R(T)','Messwerte');
+xlabel('T [°C]');
+ylabel('R [Ohm]');
+title("Exponential B-Formel");
+%Reverse Exponential
+subplot(2,1,2);
+plot(Rtlog ,Tntc,'linewidth',2);
+hold on
+scatter(Rmat,Tmat,'filled');
+hold off
+grid on
+legend('Umkehrfunktion','Messwerte');
+ylabel('T [°C]');
+xlabel('R [Ohm]');
+title("Logharitmische B-Formel T(R)");
